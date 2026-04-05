ff110a8f35fad02fa9c23fb9739fcbd644b5295d
mawya <mialfadda@alfaisal.edu>
2026-04-05 21:20:19 +0300

    ERD ActionLog, URLSubmission, Explainations, Website, and ModelVersion with their relationships added to the db

--- app/database/models.py
+++ app/database/models.py
@@ -11,6 +11,7 @@ db = SQLAlchemy()
 
 #user
 class User(db.Model):
+    #attributes
     __tablename__ = 'users'
     userID = db.Column(db.Integer, primary_key=True)
     name=db.Column(db.String(100), nullable=False)
@@ -23,6 +24,7 @@ class User(db.Model):
 
 #admin
 class Admin(db.Model):
+    #attributes
     __tablename__ = 'admins'
     adminID = db.Column(db.Integer, primary_key=True)
     status=db.Column(db.String(100), nullable=False)
@@ -38,4 +40,81 @@ class Admin(db.Model):
     approvesModelVersions=db.relationship('ModelVersion', backref='admin', lazy='True')
     recordsURLSubmissions=db.relationship('URLSubmission', backref='admin', lazy='True')
     manages=db.relationship('Manages', backref='admin', lazy='True')
+
+#ActionLog
+class ActionLog(db.Model):
+    #attributes
+    __tablename__ = 'actionLogs'
+    actionID = db.Column(db.Integer, primary_key=True)
+    action = db.Column(db.String(100), nullable=False)
+    target=db.Column(db.String(100))
+    exportType=db.Column(db.String(50))
+    creationDate = db.Column(db.DateTime,default=datetime.utcnow)
+
+    #FK
+    adminID = db.Column(db.Integer, db.ForeignKey('admins.adminID'), nullable=False)
+
+#UrlSubmission
+class URLSubmission(db.Model):
+    __tablename__ = 'urlSubmissions'
+    submissionID = db.Column(db.Integer, primary_key=True)
+    url=db.Column(db.String(3000), nullable=False)
+    creationDate = db.Column(db.DateTime,default=datetime.utcnow)
+    submissionSource=db.Column(db.String(100))
+    status=db.Column(db.String(50), default='pending')
+
+    #FK
+    userID = db.Column(db.Integer, db.ForeignKey('users.userID'), nullable=False)
+    adminID = db.Column(db.Integer, db.ForeignKey('admins.adminID'))
+
+    #relationships
+    websites=db.relationship('Website', backref='submission', lazy='True')
+
+#explaination
+class Explainations(db.Model):
+    __tablename__ = 'explainations'
+
+    explainationID = db.Column(db.Integer, primary_key=True)
+    rationale=db.Column(db.Text)
+    method=db.Column(db.String(50))
+    creationTime=db.Column(db.DateTime,default=datetime.utcnow)
+
+    #FK
+    predictionID = db.Column(db.Integer, db.ForeignKey('predictions.predictionID'), nullable=False)
+
+    #Relationships
+    prediction=db.relationship('Prediction', backref='explaination', lazy='True')
+
+#website
+class Website(db.Model):
+    __tablename__ = 'websites'
+
+    websiteID = db.Column(db.Integer, primary_key=True)
+    rootDomain=db.Column(db.String(300))
+    topLevelDomain=db.Column(db.String(300))
+
+    #FK
+    submissionID = db.Column(db.Integer, db.ForeignKey('urlSubmissions.submissionID'), nullable=False)
+
+    #relationships
+    sandboxSessions=db.relationship('SandboxSession', backref='website', lazy='True')
+
+#modelversion
+class ModelVersion(db.Model):
+    __tablename__ = 'modelVersions'
+
+    versionID = db.Column(db.Integer, primary_key=True)
+    versionTag=db.Column(db.String(100))
+    status=db.Column(db.String(100), default='pending')
+    accuracy=db.Column(db.Float)
+    creationTimeStamp = db.Column(db.DateTime,default=datetime.utcnow)
+
+    #FK
+    modelID = db.Column(db.Integer, db.ForeignKey('models.modelID'), nullable=False)
+    adminID = db.Column(db.Integer, db.ForeignKey('admins.adminID'))
+
+    #relationships
+    model=db.relationship('Model', backref='version', lazy='True')
+    predictions=db.relationship('Prediction', backref='version', lazy='True')
+
 
