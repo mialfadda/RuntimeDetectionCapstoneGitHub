bec72964ef962a452a502b754902b21f998574f0
mawya <mialfadda@alfaisal.edu>
2026-04-15 13:56:24 +0300

    Step 9: Flask-Migrate initialized - initial migration created

--- run.py
+++ run.py
@@ -1,10 +1,6 @@
-from dotenv import load_dotenv
-
-load_dotenv()
-
 from app import create_app
 
 app = create_app()
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
